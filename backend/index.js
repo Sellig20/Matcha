f221e0137b32d4aec28b92078651432c8d6d0414
Jeanne Colmou <jeannecolmou@macbook-pro-de-jeanne.home>
2024-05-27 17:15:55 +0200

    branch jeanne

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/api', (req, res) => res.send('Hello World!'));
+app.get('/apiExpress', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.get('/apiExpress/chat', (req, res) => {
+  res.send('Chat page !')
+});
 
 app.listen(3000, () => console.log(`App running on port 3000.`));
