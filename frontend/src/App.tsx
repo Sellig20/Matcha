7fe9b4e5b048caa3822de2e7bb45e259457c271b
Adrien GUILLARD <aguillar@f2r6s3.clusters.42paris.fr>
2025-06-07 11:00:24 +0200

    fix(front): sync with integ front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,86 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/assets/styles/App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useAuth } from './security/useAuth';
-import { AuthProvider } from './security/authContext';
-import { ProfileProvider, useProfile } from './components/Navbar/User/profileContext';
-import UserSettings from './components/Sidebar/UserSettings';
-import UserSignup from './components/Navbar/User/UserSignup';
-import UserSignIn from './components/Navbar/User/UserSignIn';
-import ProtectedRoute from './security/ProtectedRoute';
-import Navbar from './components/Navbar/Navbar';
-import FameRating from './components/Navbar/FameRating';
-import Sidebar from './components/Sidebar/Sidebar';
-import Chat from './components/Navbar/Chat';
-import Match from './components/Navbar/Match';
-import LandingPage from './components/LandingPage';
-import UserProfile from './components/Navbar/User/UserProfile';
-import UserProfileDisplay from './components/Navbar/User/UserProfileDisplay';
-import MatchaProfile from './components/Navbar/User/MatchaProfile';
-import Map from './components/Navbar/Map';
-import ConfidentialityPolitic from './components/Sidebar/ConfidentialityPolitic';
-import Report from './components/Sidebar/Report';
-import UserProfileUpdate from './components/Navbar/User/UserProfileUpdate';
-import UserSettingsUpdate from './components/Sidebar/UserSettingsUpdate';
-import AllUSers from './components/Navbar/AllUsers';
-import UserProduct from './components/Navbar/UserProduct';
-import { WebSocketProvider } from './security/wsContext';
-import { useEffect, useState } from 'react';
-import axiosInstance from './security/axiosInstance';
-
-const AuthWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { isAuthenticated } = useAuth();
-  return isAuthenticated ? <>{children}</> : null;
-};
-
-const ContentComplete: React.FC = () => {
-  const { isProfileComplete } = useProfile();
-  return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} /> 
-      <Route path="/signup" element={<UserSignup />} /> 
-      <Route path="/signin" element={<UserSignIn />} />
-          <Route path="/apiServeur/mymatchaprofile/:id" element={<ProtectedRoute component={MatchaProfile} />} />
-          <Route path="/apiServeur/match" element={<ProtectedRoute component={Match} />} />
-          <Route path="/apiServeur/allusers" element={<ProtectedRoute component={AllUSers} />} />
-          <Route path="/apiServeur/userproduct/:idd" element={<ProtectedRoute component={UserProduct} />} />
-          <Route path="/apiServeur/chat" element={<ProtectedRoute component={Chat} />} />
-          <Route path="/apiServeur/fm/:idd" element={<ProtectedRoute component={FameRating} />} />
-          <Route path="/apiServeur/map" element={<ProtectedRoute component={Map} />} />
-          <Route path="/apiServeur/usersettings" element={<ProtectedRoute component={UserSettings} />} />
-          <Route path="/apiServeur/usersettings/update" element={<ProtectedRoute component={UserSettingsUpdate} />} />
-          <Route path="/apiServeur/confidentialitypolitic" element={<ProtectedRoute component={ConfidentialityPolitic} />} />
-          <Route path="/apiServeur/report" element={<ProtectedRoute component={Report} />} />
-        {/* {( isProfileComplete === false && */}
-          <Route path="/apiServeur/userprofile" element={<ProtectedRoute component={UserProfile} />} />
-        {/* )} */}
-        {( isProfileComplete === true &&
-          <Route path="/apiServeur/userprofile/display" element={<ProtectedRoute component={UserProfileDisplay} />} />
-        )}
-        {( isProfileComplete === true &&
-        <Route path="/apiServeur/userprofile/display/update" element={<ProtectedRoute component={UserProfileUpdate} />} />
-        )}
-      </Routes>
-  );
-};
-
 const App: React.FC = () => {
 
-    return (
-      <AuthProvider>
-          <WebSocketProvider>
-        <ProfileProvider>
-              <Router>
-                  <Navbar />
-                  <AuthWrapper>
-                    <Sidebar />
-                  </AuthWrapper>
-                  <ContentComplete />
-              </Router>
-        </ProfileProvider>
-          </WebSocketProvider>
-      </AuthProvider>
-    );
+  return (
+    <div>
+      Bienvenidoooos App.tsx !
+    </div>
+  );
 };
 
 export default App;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +12,6 @@ export default defineConfig({
     hmr: {
       port: 3001,
       host: "localhost",
-    }, 
-  }
-})
+    },
+  },
+});
